c249d9587c180bf3aa781322507464c8f02e9a3a
Aeroz69 <72295917+Aeroz69@users.noreply.github.com>
2021-03-04 11:25:43 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import asyncio
+import os
+import uvloop
+import sys
+
+if not os.getenv('DEVICE_ID') and \
+        not os.getenv('ACCOUNT_ID') and \
+        not os.getenv('SECRET'):
+    print("Please paste your device auths into the \".env\" file.\n"
+          "If you're confused, re-watch the tutorial.")
+    sys.exit()
+
+os.system('pip install -U Aerozbot')
+os.system('clear')
+
+import Aerozbot
+
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+client = Aerozbot.PartyBot(
+    device_id=os.getenv('DEVICE_ID'),
+    account_id=os.getenv('ACCOUNT_ID'),
+    secret=os.getenv('SECRET')
+)
+
+try:
+    client.run()
+except Exception as e:
+    print(e)
+    print("Failed to login, your device auths are probably invalid, please "
+          "try again and make new ones.\nIf you're confused, re-watch the "
+          "tutorial.")
